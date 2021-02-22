65f39b4c6814986b88efc1fa36bbc443828e18a9
Artur Galimskii <artur.galimskii@varwin.com>
2021-02-22 13:58:48 +0500

    init

---
+++ Options.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+
+namespace todo_list
+{
+
+    [Verb("add")]
+    class AddOptions
+    {
+        [Option('d',Required = false, HelpText = "Date of current task")]
+        public string Date { get; set; }
+
+        [Option('t',HelpText = "Task text", Default=null)]
+        public string Text {get; set;}
+    }
+
+    [Verb("list")]
+    public class ListOptions
+    {
+        // [Option("List", HelpText = "List all tasks", Default = null)]
+        // public string List {get; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace todo_list
+{
+    class Program
+    {
+        private static List<string> TaskList = new List<string>()
+        {
+            "Task1", "task2", "Task3"
+        };
+
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<AddOptions, ListOptions>(args)
+                .WithParsed<AddOptions>(RunAddOptions)
+                .WithParsed<ListOptions>(RunListOptions)
+                .WithNotParsed(HandleParseError);
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+            if (errs.IsVersion())
+            {                
+                return;
+            }
+
+            if (errs.IsHelp())
+            {                
+                return;
+            }
+            Console.WriteLine("Parser Fail");
+        }
+
+        private static void RunAddOptions(AddOptions opts)
+        {
+            System.Console.WriteLine("Add task is: " + opts.Text);
+            System.Console.WriteLine("Date of task is: " + opts.Date);       
+        }
+
+        private static void RunListOptions(ListOptions opts)
+        {               
+            foreach(var task in TaskList)
+            {
+                System.Console.WriteLine("task is : " + task);
+            }
+        }
+    }
+}
